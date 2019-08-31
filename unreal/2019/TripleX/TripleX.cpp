3344613372e49e8cc519f8811cacce16a37a3627
Caleb <bulletproofchild@gmail.com>
2019-08-31 17:22:17 -0600

    create PlayerGuess variable

--- unreal/2019/TripleX/TripleX.cpp
+++ unreal/2019/TripleX/TripleX.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-  // PRINT WELCOME MESSAGE TO THE TERMINAL
+  // Print welcome message to the terminal 
   std::cout << std::endl << "You are wetboy assassin hired by the Sa'kage to infiltrate the castle...";
   std::cout << std::endl;
   std::cout << "In order to enter the first door you must pick the lock..." << std::endl;
 
-  // DECLARE 3 NUMBER CODE
+  // Declare the 3 number code
   const int CodeA = 4;
   const int CodeB = 3;
   const int CodeC = 2;
@@ -15,12 +15,14 @@ int main()
   const int CodeSum = CodeA + CodeB + CodeC;
   const int CodeProduct = CodeA * CodeB * CodeC;
 
-  // PRINT SUM & PRODUCT TO THE TERMINAL
+  // Print CodeSum & CodeProduct to the terminal
   std::cout << std::endl;
   std::cout << "There are 3 numbers in the code" << std::endl;
   std::cout << "The codes add-up to: " << CodeSum << std::endl;
   std::cout << "The codes multiply to give: " << CodeProduct << std::endl;
 
-  // END GAME
+  int PlayerGuess;
+
+  // End game
   return 0;
 }
