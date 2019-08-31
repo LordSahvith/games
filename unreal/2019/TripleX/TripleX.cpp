9d2487d5114ff8659ffabb5db6a161e458df206b
Caleb <bulletproofchild@gmail.com>
2019-08-31 16:43:07 -0600

    comments in c++

--- unreal/2019/TripleX/TripleX.cpp
+++ unreal/2019/TripleX/TripleX.cpp
@@ -2,19 +2,28 @@
 
 int main()
 {
+  // PRINT WELCOME MESSAGE TO THE TERMINAL
   std::cout << "You are wetboy assassin hired by the Sa'kage to infiltrate the castle...";
   std::cout << std::endl;
   std::cout << "In order to enter the first door you must pick the lock...";
 
+  // DECLARE 3 NUMBER CODE
   int a = 4;
   int b = 3;
   int c = 2;
 
+  /*
+    MULIT LINE 
+    COMMENT
+  */
   int sum = a + b + c;
   int product = a * b * c;
 
+  // PRINT SUM & PRODUCT TO THE TERMINAL
   std::cout << std::endl;
   std::cout << a << std::endl;
   std::cout << product << std::endl;
+
+  // END GAME
   return 0;
 }
