ea9869ba8fa0bffe58544d806dd4900a6e81a3a2
Caleb <bulletproofchild@gmail.com>
2019-08-31 16:32:28 -0600

    learn c++ variables

--- unreal/2019/TripleX/TripleX.cpp
+++ unreal/2019/TripleX/TripleX.cpp
@@ -5,5 +5,16 @@ int main()
   std::cout << "You are wetboy assassin hired by the Sa'kage to infiltrate the castle...";
   std::cout << std::endl;
   std::cout << "In order to enter the first door you must pick the lock...";
+
+  int a = 4;
+  int b = 3;
+  int c = 2;
+
+  int sum = a + b + c;
+  int product = a * b * c;
+
+  std::cout << std::endl;
+  std::cout << a << std::endl;
+  std::cout << product << std::endl;
   return 0;
 }
