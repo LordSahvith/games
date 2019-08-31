76b298f9fc0fdb4c9bf6a7183f96efff06920888
Caleb <bulletproofchild@gmail.com>
2019-08-31 17:18:50 -0600

    turn variables into constants and add more info to game

--- unreal/2019/TripleX/TripleX.cpp
+++ unreal/2019/TripleX/TripleX.cpp
@@ -3,26 +3,23 @@
 int main()
 {
   // PRINT WELCOME MESSAGE TO THE TERMINAL
-  std::cout << "You are wetboy assassin hired by the Sa'kage to infiltrate the castle...";
+  std::cout << std::endl << "You are wetboy assassin hired by the Sa'kage to infiltrate the castle...";
   std::cout << std::endl;
-  std::cout << "In order to enter the first door you must pick the lock...";
+  std::cout << "In order to enter the first door you must pick the lock..." << std::endl;
 
   // DECLARE 3 NUMBER CODE
-  int a = 4;
-  int b = 3;
-  int c = 2;
+  const int CodeA = 4;
+  const int CodeB = 3;
+  const int CodeC = 2;
 
-  /*
-    MULIT LINE 
-    COMMENT
-  */
-  int sum = a + b + c;
-  int product = a * b * c;
+  const int CodeSum = CodeA + CodeB + CodeC;
+  const int CodeProduct = CodeA * CodeB * CodeC;
 
   // PRINT SUM & PRODUCT TO THE TERMINAL
   std::cout << std::endl;
-  std::cout << a << std::endl;
-  std::cout << product << std::endl;
+  std::cout << "There are 3 numbers in the code" << std::endl;
+  std::cout << "The codes add-up to: " << CodeSum << std::endl;
+  std::cout << "The codes multiply to give: " << CodeProduct << std::endl;
 
   // END GAME
   return 0;
