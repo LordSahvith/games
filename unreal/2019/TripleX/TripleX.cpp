e1d18dc2b8bc13c74274da5bcc525b09cd897eb1
Caleb <bulletproofchild@gmail.com>
2019-08-31 19:30:53 -0600

    add in door ascii art to simulate a door for each level

--- unreal/2019/TripleX/TripleX.cpp
+++ unreal/2019/TripleX/TripleX.cpp
@@ -18,10 +18,39 @@ void Introduction()
   std::cout << "  | |  _ __ | |_ _| | |_ _ __ __ _| |_ _  ___  _ __  \n";
   std::cout << "  | | | '_ \\|  _| | | __| '__/ _` | __| |/ _ \\| '_ \\ \n";
   std::cout << " _| |_| | | | | | | | |_| | | (_| | |_| | (_) | | | |\n";
-  std::cout << "|_____|_| |_|_| |_|_|\\__|_|  \\__,_|\\__|_|\\___/|_| |_|\n\n";
+  std::cout << "|_____|_| |_|_| |_|_|\\__|_|  \\__,_|\\__|_|\\___/|_| |_|\n\n\n\n";
   std::cout << "You're a Wetboy Assassin hired by the Sa'kage.\n";
   std::cout << "They want you to rid the country of the insulent King.\n";
-  std::cout << "Your task is simple...you must infiltrate the castle,\nfind the King unnoticed and kill him.\n\n";
+  std::cout << "Your task is simple...infiltrate the castle,\n";
+  std::cout << "find the King unnoticed & kill him.\n\n";
+}
+
+void DoorCreate(int Level)
+{
+  if (Level < 4)
+  {
+    std::cout << " ____________\n";
+    std::cout << "|  ________  |\n";
+    std::cout << "| |        | |      ___    ___    ___\n";
+    std::cout << "| |        | |     |   |  |   |  |   |\n";
+    std::cout << "| |   " << Level << "  ()| |     | 1 |  | 2 |  | 3 |\n";
+    std::cout << "| |        | |     |___|  |___|  |___|\n";
+    std::cout << "| |        | |\n";
+    std::cout << "| |________| |\n";
+    std::cout << "|____________|\n\n";
+  }
+  else
+  {
+    std::cout << " ____________\n";
+    std::cout << "|  ________  |\n";
+    std::cout << "| |        | |      ___    ___    ___\n";
+    std::cout << "| |        | |     |   |  |   |  |   |\n";
+    std::cout << "| | /\\_/\\()| |     | 1 |  | 2 |  | 3 |\n";
+    std::cout << "| | |___|  | |     |___|  |___|  |___|\n";
+    std::cout << "| |        | |\n";
+    std::cout << "| |________| |\n";
+    std::cout << "|____________|\n\n";
+  }
 }
 
 void Levels(int Level, int Codes)
@@ -40,22 +69,23 @@ void Levels(int Level, int Codes)
   {
     std::cout << "This door has a level of " << Level << ".\n";
   }
-  std::cout << "You must pick the lock by guessing the " << Codes << " codes...\n\n";
+  std::cout << "You must pick the lock by guessing the " << Codes << " codes...\n";
+  DoorCreate(Level);
 }
 
-bool PlayGame(int Level)
+bool PlayGame(int Level, int NumberOfCodes)
 {
   if (Level == 1)
   {
     Introduction();
   }
 
-  Levels(Level, 3);
+  Levels(Level, NumberOfCodes);
 
   // Declare the 3 number code
-  const int CodeA = 4;
+  const int CodeA = 3;
   const int CodeB = 3;
-  const int CodeC = 2;
+  const int CodeC = 3;
 
   const int CodeSum = CodeA + CodeB + CodeC;
   const int CodeProduct = CodeA * CodeB * CodeC;
@@ -95,15 +125,22 @@ bool PlayGame(int Level)
 int main()
 {
   int LevelDifficulty = 1;
-  while (LevelDifficulty <= 4)
+  int NumberOfCodes = 3;
+  const int MaxDifficulty = 4;
+
+  while (LevelDifficulty <= MaxDifficulty) // loop through game until last level
   {
-    bool bLevelComplete = PlayGame(LevelDifficulty);
+    bool bLevelComplete = PlayGame(LevelDifficulty, NumberOfCodes);
     std::cin.clear();  // clears any errors
     std::cin.ignore(); // discards the buffer
 
     if (bLevelComplete)
     {
       ++LevelDifficulty;
+    }
+    else
+    {
+      LevelDifficulty = 1;
     }
   }
 
