7b32615a75c9e5c3d8c615a0d9291b17ecb0a528
Caleb <bulletproofchild@gmail.com>
2019-09-01 23:15:57 -0600

    store users guess

--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
@@ -3,9 +3,22 @@
 
 int main()
 {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
+
 	std::cout << "Welcome to Bulls N Cows!\n";
 	std::cout << "Can you guess the " << WORD_LENGTH;
-	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << " letter isogram I'm thinking of?\n\n";
+
+	// get a guess from the player
+	std::string Guess = "";
+	std::cout << "Enter guess: ";
+	std::cin >> Guess;
+
+	// repeat the guess back to them
+	std::cout << "Your guess: " << Guess;
+
+
+	std::cout << std::endl;
 	return 0;
 }
