58a1a8bb60e5114663948ab58b76d0f94a08eb2c
Caleb <bulletproofchild@gmail.com>
2019-09-01 23:49:18 -0600

    create functions and use for loop

--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
+#include <string>
 
+void PrintIntro();
+std::string GetGuessAndPrintBack();
 
 int main()
 {
-	// introduce the game
+	PrintIntro();
+
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 0; count < NUMBER_OF_TURNS; count++) {
+		GetGuessAndPrintBack();
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENGTH = 5;
 
 	std::cout << "Welcome to Bulls N Cows!\n";
 	std::cout << "Can you guess the " << WORD_LENGTH;
 	std::cout << " letter isogram I'm thinking of?\n\n";
+	return;
+}
 
-	// get a guess from the player
+// get a guess from the player
+std::string GetGuessAndPrintBack()
+{
 	std::string Guess = "";
 	std::cout << "Enter guess: ";
-	std::cin >> Guess;
+	std::getline(std::cin, Guess);
 
 	// repeat the guess back to them
-	std::cout << "Your guess: " << Guess;
-
-
-	std::cout << std::endl;
-	return 0;
+	std::cout << "Your guess: " << Guess << std::endl;
+	return Guess;
 }
