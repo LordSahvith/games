349f9655273bce379069061b0eaf9bfe922a3c5b
Caleb <bulletproofchild@gmail.com>
2019-09-02 00:07:05 -0600

    code better way for printing guess

--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
@@ -18,8 +18,8 @@ void PlayGame()
 	// loop for the number of turns asking for guesses
 	constexpr int NUMBER_OF_TURNS = 5;
 	for (int count = 0; count < NUMBER_OF_TURNS; count++) {
-		GetGuess();
-		std::cout << std::endl;
+		std::string Guess = GetGuess();
+		std::cout << "Your guess: " << Guess << std::endl << std::endl;
 	}
 	std::cout << std::endl;
 }
@@ -41,12 +41,5 @@ std::string GetGuess()
 	std::string Guess = "";
 	std::cout << "Enter guess: ";
 	std::getline(std::cin, Guess);
-	PrintGuess(Guess);
 	return Guess;
-}
-
-// repeat the guess back to them
-void PrintGuess(std::string Guess)
-{
-	std::cout << "Your guess: " << Guess << std::endl;
 }
