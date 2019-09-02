66a60c35722f29f61e1be2608d837f36feea1cb1
Caleb <bulletproofchild@gmail.com>
2019-09-01 23:09:11 -0600

    learn about constexpr

--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+
+
 int main()
 {
+	constexpr int WORD_LENGTH = 5;
+	std::cout << "Welcome to Bulls N Cows!\n";
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram I'm thinking of?\n";
 	return 0;
 }
