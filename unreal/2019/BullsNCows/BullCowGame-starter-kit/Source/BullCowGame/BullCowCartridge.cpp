959b9087b90e1d0be189165f81f84b33b8c7e1f7
Caleb <bulletproofchild@gmail.com>
2019-09-01 18:04:45 -0600

    psuedo code start of game

--- unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,16 +3,25 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    // welcoming player 
     Super::BeginPlay();
     PrintLine(TEXT("Howdy Partner!"));
     PrintLine(TEXT("Guess the 6 letter word")); // Magic number remove!
     PrintLine(TEXT("Press enter to continue..."));
-    HiddenWord = TEXT("naruto"); // move outside this function
+
+    // setup game
+    HiddenWord = TEXT("naruto"); // get isogram
+    // set lives
+
+    // prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
   ClearScreen();
+
+  // checking PlayerGuessd
+
   PrintLine(Input);
 
   if (Input == HiddenWord) {
@@ -21,4 +30,19 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
   else {
     PrintLine(TEXT("Maybe next time."));
   }
+
+  // check if isogram
+  // prompt to guess again
+  // check right number of characters
+  // prompt to guess again
+
+  // remove life
+
+  // check if lives > 0
+  // if yes GuessAgain
+  // Show lives left
+  // if no show GameOver & HiddenWord
+  // Prompt to Play Again, press enter to play again? 
+  // check user input
+  // PlayAgain or quit
 }
