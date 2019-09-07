d97136320b9580ef7c069631fea18808869dc549
Caleb <bulletproofchild@gmail.com>
2019-09-07 15:28:15 -0600

    use a struct to to help with validating the guess

--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/FBullsNCows.cpp
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/FBullsNCows.cpp
@@ -21,6 +21,8 @@ void FBullCowGame::Reset()
   constexpr int32 MAX_TRIES = 3;
   MyMaxTries = MAX_TRIES;
 
+  const FString HIDDEN_WORD = "planet";
+  MyHiddenWord = HIDDEN_WORD;
   MyCurrentTry = 1;
 	return;
 }
@@ -28,4 +30,18 @@ void FBullCowGame::Reset()
 bool FBullCowGame::CheckGuessValidity(FString)
 {
 	return false;
+}
+
+// receives a VALID guess, increments turn, and returns count
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+  // increment the turn number
+  MyCurrentTry++;
+
+  // setup a return variable
+  BullCowCount BullCowCount;
+
+  // loop through all letter in the guess
+    // compare letters against the hidden word
+  return BullCowCount;
 }
--- unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/FBullsNCows.h
+++ unreal/2019/BullsNCowsTerminal/BullsNCowsTerminal/BullsNCows/FBullsNCows.h
@@ -4,20 +4,29 @@
 using FString = std::string;
 using int32 = int;
 
-class FBullCowGame {
+// all values intialized to 0
+struct BullCowCount
+{
+  int32 Bulls = 0;
+  int32 Cows = 0;
+};
+
+class FBullCowGame
+{
 public:
   FBullCowGame(); // constructor
 
-	int32 GetMaxTries() const;
-	int32 GetCurrentTry() const;
-	bool IsGameWon() const;
+  int32 GetMaxTries() const;
+  int32 GetCurrentTry() const;
+  bool IsGameWon() const;
 
-	void Reset(); // TODO  make a more rich return value
-	bool CheckGuessValidity(FString); // TODO make a more rich return value
+  void Reset();                     // TODO  make a more rich return value
+  bool CheckGuessValidity(FString); // TODO make a more rich return value
+  BullCowCount SubmitGuess(FString);
 
 private:
   // see contructor for initialization
-	int32 MyCurrentTry;
-	int32 MyMaxTries;
-  // provide a method for counting bulls and cows, and increading turn #
+  int32 MyCurrentTry;
+  int32 MyMaxTries;
+  FString MyHiddenWord;
 };
