9f637e2faa2e892556d165d69f2989adbbfdd0f3
Caleb <bulletproofchild@gmail.com>
2019-09-08 21:19:08 -0600

    add position report component to the chair and table and print to log with object name

--- unreal/2019/BuildingEscpae/Source/BuildingEscpae/PositionReport.cpp
+++ unreal/2019/BuildingEscpae/Source/BuildingEscpae/PositionReport.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -9,8 +10,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,8 +18,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!"), *ObjectName);	
 }
 
 
