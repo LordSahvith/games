375c1c45995cc75366530f72b2f4b3577f3727c8
Caleb <bulletproofchild@gmail.com>
2020-02-15 15:59:52 -0700

    get bulls and cows and show them to the player

--- unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -82,6 +82,11 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
     }
 
     // show the player bulls and cows
+    int32 Bulls, Cows; // OUT variables
+    GetBullCows(Guess, Bulls, Cows);
+
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Bulls, Cows);
+
     PrintLine(TEXT("Sorry, try again! \nYou have %i lives left."), Lives);
 }
 
@@ -114,4 +119,33 @@ TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList
     }
 
     return ValidWords;
+}
+
+void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int32& CowCount) const
+{
+    BullCount = 0;
+    CowCount = 0;
+
+    // for the index of Guess is same as index Hidden, BullCount++
+    // if not a bull was it a cow? if yes CowCount++
+
+    for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
+    {
+        if (Guess[GuessIndex] == HiddenWord[GuessIndex])
+        {
+            BullCount++;
+            continue;
+        }
+
+        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); HiddenIndex++)
+        {
+            if(Guess[GuessIndex] == HiddenWord[HiddenIndex])
+            {
+                CowCount++;
+                break;
+            }
+        }
+        
+    }
+    
 }
