a3df578a92c633aac5b712be2067ea90df0bdd04
Caleb <bulletproofchild@gmail.com>
2020-02-15 16:11:40 -0700

    update using OUT parameters to using a struct

--- unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ unreal/2019/BullsNCows/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -82,10 +82,9 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
     }
 
     // show the player bulls and cows
-    int32 Bulls, Cows; // OUT variables
-    GetBullCows(Guess, Bulls, Cows);
+    FBullCowCount Score = GetBullCows(Guess);
 
-    PrintLine(TEXT("You have %i Bulls and %i Cows"), Bulls, Cows);
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
 
     PrintLine(TEXT("Sorry, try again! \nYou have %i lives left."), Lives);
 }
@@ -121,19 +120,15 @@ TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList
     return ValidWords;
 }
 
-void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int32& CowCount) const
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
 {
-    BullCount = 0;
-    CowCount = 0;
-
-    // for the index of Guess is same as index Hidden, BullCount++
-    // if not a bull was it a cow? if yes CowCount++
+    FBullCowCount Count;
 
     for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
     {
         if (Guess[GuessIndex] == HiddenWord[GuessIndex])
         {
-            BullCount++;
+            Count.Bulls++;
             continue;
         }
 
@@ -141,11 +136,12 @@ void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int3
         {
             if(Guess[GuessIndex] == HiddenWord[HiddenIndex])
             {
-                CowCount++;
+                Count.Cows++;
                 break;
             }
         }
         
     }
-    
+
+    return Count;    
 }
