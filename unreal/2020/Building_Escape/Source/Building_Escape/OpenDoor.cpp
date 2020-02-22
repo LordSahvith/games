27b018d26a1058ac4be2b68a0de5d087f1f6e17c
Caleb <bulletproofchild@gmail.com>
2020-02-22 14:18:28 -0700

    get pressure plates to activate to certain amount of weight

--- unreal/2020/Building_Escape/Source/Building_Escape/OpenDoor.cpp
+++ unreal/2020/Building_Escape/Source/Building_Escape/OpenDoor.cpp
@@ -1,31 +1,31 @@
 // Copyright 2020 Lord Sahvith
 
 #include "OpenDoor.h"
+#include "Components/PrimitiveComponent.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
 
+#define OUT
+
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 }
-
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
     InitialYaw = GetOwner()->GetActorRotation().Yaw;
     CurrentYaw = InitialYaw;
     OpenAngle += InitialYaw;
 
-    if (!PressurePlate) 
+    if (!PressurePlate)
     {
         UE_LOG(LogTemp, Error, TEXT("%s has no pressure plate assigned!"), *GetOwner()->GetName());
     }
@@ -33,39 +33,54 @@ void UOpenDoor::BeginPlay()
     ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
-
 // Called every frame
-void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
+    if (TotalMassOfActors() > MassToOpenDoors)
     {
-       OpenDoor(DeltaTime);
-       DoorLastOpened = GetWorld()->GetTimeSeconds();
+        OpenDoor(DeltaTime);
+        DoorLastOpened = GetWorld()->GetTimeSeconds();
     }
-    else 
+    else
     {
         if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorCloseDelay)
         {
             CloseDoor(DeltaTime);
         }
-    }    
+    }
 }
 
 void UOpenDoor::OpenDoor(float DeltaTime)
 {
     CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
-    FRotator DoorRotation = GetOwner()-> GetActorRotation();
+    FRotator DoorRotation = GetOwner()->GetActorRotation();
     DoorRotation.Yaw = CurrentYaw;
-    GetOwner()->SetActorRotation(DoorRotation);  
+    GetOwner()->SetActorRotation(DoorRotation);
 }
 
 void UOpenDoor::CloseDoor(float DeltaTime)
 {
     CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
-    FRotator DoorRotation = GetOwner()-> GetActorRotation();
+    FRotator DoorRotation = GetOwner()->GetActorRotation();
     DoorRotation.Yaw = CurrentYaw;
     GetOwner()->SetActorRotation(DoorRotation);
 }
 
+float UOpenDoor::TotalMassOfActors() const
+{
+    float TotalMass = 0.f;
+
+    // find all overlapping actors
+    TArray<AActor*> OverlappingActors;
+    PressurePlate->GetOverlappingActors(OUT OverlappingActors);
+
+    // add up their masses
+    for (AActor* Actor : OverlappingActors)
+    {
+        TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+    }
+
+    return TotalMass;
+}
